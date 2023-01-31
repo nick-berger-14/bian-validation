24708424138ce4eb485dc51d5b15369aa29762bb
Bryan Cross <bryanrkcross@gmail.com>
2023-01-31 14:10:40 -0600

    Organizing resources, finishing example (index.js)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Ajv = require('ajv');
 
   
 
-var item = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/bian-validation/resources/generated-schema.json', 'utf8'));
+var item = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/bian-validation/resources/postman/PaymentInitiationTransaction-object-schema.json', 'utf8'));
 
 /*
   const fastifyRouteConfig = {
@@ -20,28 +20,43 @@ var item = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/
   function requireAll (schema) {
     var newSchema = {};
     newSchema.type = schema.type;
-    newSchema.properties = schema.schema.properties;
+    newSchema.properties = schema.properties;
     //schema.schema.required = Object.keys(schema.schema.properties);
-    newSchema.required = Object.keys(schema.schema.properties);
+    newSchema.required = Object.keys(schema.properties);
     newSchema.additionalProperties = false;
     //schema.schema.properties.PaymentInitiationTransaction.required = Object.keys(schema.schema.properties.PaymentInitiationTransaction.properties);
-    newSchema.properties.PaymentInitiationTransaction.required = Object.keys(schema.schema.properties.PaymentInitiationTransaction.properties);
+    newSchema.properties.PaymentInitiationTransaction.required = Object.keys(schema.properties.PaymentInitiationTransaction.properties);
     newSchema.properties.PaymentInitiationTransaction.additionalProperties = false;
     return newSchema
   }
 
-  var data = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/bian-validation/resources/PaymentInitiationTransaction-bad.json', 'utf8'));
-
+  function validate(data, schema) {
+        const ajv = new Ajv();
+        schema = requireAll(schema);
+        const validate = ajv.compile(schema);
+        const valid = validate(data);
+        return valid;
+  }
   
+  var data = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/bian-validation/resources/response-bodies/PaymentInitiationTransaction-bad.json', 'utf8'));
+  valid = validate(data, item);
+  console.log('-bad body is valid?: ' + valid);
+
+
+  var data = JSON.parse(fs.readFileSync('/Users/bryancross/dev/github/bidnessforb/bian-validation/resources/response-bodies/PaymentInitiationTransaction-good.json', 'utf8'));
+  valid = validate(data, item);
+  console.log('-good body is valid?: ' + valid);
+  /*
         const ajv = new Ajv();
         item = requireAll(item);
         console.log(JSON.stringify(item));
         const validate = ajv.compile(item);
         const valid = validate(data);
-        
+        */
+
 
   
-  console.log(valid);
+  
 
 
 
