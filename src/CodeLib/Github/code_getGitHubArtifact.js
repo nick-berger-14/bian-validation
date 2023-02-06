b1aaf7b85dba031999f744bdfca9b4abfb582589
Bryan Cross <bryanrkcross@gmail.com>
2023-02-05 20:32:25 -0600

    Removing workspace

--- src/CodeLib/Github/code_getGitHubArtifact.js
+++ src/CodeLib/Github/code_getGitHubArtifact.js
@@ -4,9 +4,8 @@
  * environment variable
 */
 //Setup the URL components
-
-var ghci = JSON.parse(pm.collectionVariables.get('ghci'));
-console.log (ghci.files);
+var ghci = JSON.parse(pm.collectionVariables.get('gh_config'));
+console.log(ghci);
 token = pm.environment.get("GitHub_PAT");
 for(i = 0; i < ghci.files.length; i++)
 {
@@ -14,11 +13,8 @@ for(i = 0; i < ghci.files.length; i++)
     //var baseUrl = pm.collectionVariables.get("GitHub_APIBaseURL");
     baseUrl = 'https://api.github.com'
     //var ref = pm.collectionVariables.get("GitHub_currentBranch");
-    console.log("REF: " + file.ref);
-    console.log("REF UNDEF: " + (file.ref === ''));
     //Build the URL.  If ref is defined and not '', append it to pull from the specific ref (branch)
     var url = baseUrl + "/repos/" + ghci.slug + "/contents/" + file.path + ((file.ref === undefined || file.ref === null || file.ref === '') ? '' : '?ref=' + file.ref);
-    console.log("GitHub " +file.target + " Script URL: " + url);
     const postRequest = {
     url: url,
     method: 'get',
@@ -30,19 +26,14 @@ for(i = 0; i < ghci.files.length; i++)
     sendRequest(postRequest, file.target);
 }    
     
-
-
-
 function sendRequest(postRequest, target) {
-    console.log("TARGET: " + target);
+    
     pm.sendRequest(postRequest, (err,res) =>  {
         var jsonData = JSON.parse(res.text());
         var base64content = jsonData.content;
         var buff = new Buffer(base64content, 'base64');
         var content = buff.toString('ascii');
-        console.log("Setting " + target);
+    
         pm.collectionVariables.set(target, content);
     });
 }
-
-
